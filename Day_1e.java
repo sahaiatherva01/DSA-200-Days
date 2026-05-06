d9054a74daef6e3a3b0eaf006268fc3f76307979
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-05-06 12:39:43 +0530

    Add longestMonotonicSubarray method in Solution class
    Implement a method to find the longest monotonic subarray.

---
+++ Day_1e.java
@@ -0,0 +1,38 @@
+/*
+You are given an array of integers nums. Return the length of the longest subarray of nums which is either strictly increasing or strictly decreasing.
+Example 1:
+Input: nums = [1,4,3,3,2]
+Output: 2
+Explanation:
+The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+Hence, we return 2.
+Example 2:
+Input: nums = [3,3,3,3]
+Output: 1
+Explanation:
+The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+Hence, we return 1.
+*/
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int n = nums.length;
+        int inc = 1, dec = 1;
+        int t = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                inc++;
+                dec = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                dec++;
+                inc = 1;
+            } else {
+                inc = 1;
+                dec = 1;
+            }
+            t = Math.max(t, Math.max(inc, dec));
+        }
+        return t;
+    }
+}
