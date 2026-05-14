974b8686b81770a9a24b8204e12954aadfd42a2c
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-05-14 21:23:28 +0530

    Create Day_9b.java

---
+++ Day_9b.java
@@ -0,0 +1,46 @@
+/*
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+ 
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+Explanation:
+The strings s and t can be made identical by:
+Mapping 'e' to 'a'.
+Mapping 'g' to 'd'.
+Example 2:
+Input: s = "f11", t = "b23"
+Output: false
+Explanation:
+The strings s and t can not be made identical as '1' needs to be mapped to both '2' and '3'.
+*/
+import java.util.HashMap;
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            // Check s -> t mapping
+            if (mapST.containsKey(c1)) {
+                if (mapST.get(c1) != c2) {
+                    return false;
+                }
+            } else {
+                mapST.put(c1, c2);
+            }
+            // Check t -> s mapping
+            if (mapTS.containsKey(c2)) {
+                if (mapTS.get(c2) != c1) {
+                    return false;
+                }
+            } else {
+                mapTS.put(c2, c1);
+            }
+        }
+        return true;
+    }
+}
