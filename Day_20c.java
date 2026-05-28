e7c27f117e3f1747ade40f55789e5cd33e26229b
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-05-28 22:47:32 +0530

    Create Day_20c.java

---
+++ Day_20c.java
@@ -0,0 +1,22 @@
+/*
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+A string is represented by an array if the array elements concatenated in order forms the string.
+
+Example 1:
+Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+Output: true
+Explanation:
+word1 represents string "ab" + "c" -> "abc"
+word2 represents string "a" + "bc" -> "abc"
+The strings are the same, so return true.
+Example 2:
+Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
+Output: false
+*/
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String s1 = String.join("", word1);
+        String s2 = String.join("", word2);
+        return s1.equals(s2);
+    }
+}
