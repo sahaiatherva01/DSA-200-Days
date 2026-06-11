ead0de240735133fe786f5a8570cce7b3d0cb077
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-06-11 12:52:35 +0530

    Create Day_34.java

---
+++ Day_34.java
@@ -0,0 +1,33 @@
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+*/
+import java.util.*;
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> result = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                result.add(num);
+            }
+        }
+        int[] ans = new int[result.size()];
+        int i = 0;
+        for (int num : result) {
+            ans[i++] = num;
+        }
+        return ans;
+    }
+}
