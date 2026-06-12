914ec2960392902736aa3d19361dadda720e7b5a
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-06-12 21:46:31 +0530

    Create Day_35b.java

---
+++ Day_35b.java
@@ -0,0 +1,36 @@
+/*
+You are given an m x n integer matrix matrix with the following two properties:
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+You must write a solution in O(log(m * n)) time complexity.
+
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+*/
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int left = 0;
+        int right = rows * cols - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int value = matrix[mid / cols][mid % cols];
+            if (value == target) {
+                return true;
+            }
+            if (value < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
