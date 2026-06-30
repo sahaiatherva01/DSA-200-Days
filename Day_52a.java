22e952a7f8a87b44ee4b4d65449343bbc4ff315d
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-06-30 20:16:40 +0530

    Add maxFrequency method to calculate element frequency
    Implement a solution to find the maximum frequency of an element in an array after at most k increment operations.

---
+++ Day_52a.java
@@ -0,0 +1,40 @@
+/*
+The frequency of an element is the number of times it occurs in an array.
+You are given an integer array nums and an integer k. In one operation, you can choose an index of nums and increment the element at that index by 1.
+Return the maximum possible frequency of an element after performing at most k operations.
+
+Example 1:
+Input: nums = [1,2,4], k = 5
+Output: 3
+Explanation: Increment the first element three times and the second element two times to make nums = [4,4,4].
+4 has a frequency of 3.
+
+Example 2:
+Input: nums = [1,4,8,13], k = 5
+Output: 2
+Explanation: There are multiple optimal solutions:
+- Increment the first element three times to make nums = [4,4,8,13]. 4 has a frequency of 2.
+- Increment the second element four times to make nums = [1,8,8,13]. 8 has a frequency of 2.
+- Increment the third element five times to make nums = [1,4,13,13]. 13 has a frequency of 2.
+
+Example 3:
+Input: nums = [3,9,6], k = 2
+Output: 1
+*/
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        long sum = 0;
+        int left = 0;
+        int ans = 1;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while ((long) nums[right] * (right - left + 1) - sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
+}
