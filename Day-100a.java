ea8a2186ce007b63c86f59bc83619c9c5e96efea
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-08-17 09:01:01 +0530

    Create Day-100a.java

---
+++ Day-100a.java
@@ -0,0 +1,38 @@
+/*
+Given 3 positives numbers a, b and c. Return the minimum flips required in some bits of a and b to make ( a OR b == c ). (bitwise OR operation).
+Flip operation consists of change any single bit 1 to 0 or change the bit 0 to 1 in their binary representation.
+
+Example 1:
+Input: a = 2, b = 6, c = 5
+Output: 3
+Explanation: After flips a = 1 , b = 4 , c = 5 such that (a OR b == c)
+
+Example 2:
+Input: a = 4, b = 2, c = 7
+Output: 1
+
+Example 3:
+Input: a = 1, b = 2, c = 3
+Output: 0
+*/
+class Solution {
+    public int minFlips(int a, int b, int c) {
+        int ans = 0;
+        while (a > 0 || b > 0 || c > 0) {
+            int x = a & 1;
+            int y = b & 1;
+            int z = c & 1;
+            if (z == 0) {
+                ans += x + y;
+            } else {
+                if (x == 0 && y == 0) {
+                    ans++;
+                }
+            }
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+        return ans;
+    }
+}
