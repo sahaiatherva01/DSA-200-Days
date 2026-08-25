9508cd4eabab82c9a3208f19fcdd46a6a4f4997d
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-08-25 12:38:44 +0530

    Create Day-108c.java

---
+++ Day-108c.java
@@ -0,0 +1,29 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+*/
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+        for (int x : freq) {
+            if (x != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
