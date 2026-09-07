b8a7add8d285b87e8c4a97532fd7a6fc87cb13c1
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-09-07 06:46:14 +0530

    Create Day_121.java

---
+++ Day_121.java
@@ -0,0 +1,33 @@
+/*
+Given a string s, return the number of distinct non-empty subsequences of s. Since the answer may be very large, return it modulo 109 + 7.
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not.
+
+Example 1:
+Input: s = "abc"
+Output: 7
+Explanation: The 7 distinct subsequences are "a", "b", "c", "ab", "ac", "bc", and "abc".
+
+Example 2:
+Input: s = "aba"
+Output: 6
+Explanation: The 6 distinct subsequences are "a", "b", "ab", "aa", "ba", and "aba".
+
+Example 3:
+Input: s = "aaa"
+Output: 3
+Explanation: The 3 distinct subsequences are "a", "aa" and "aaa".
+*/
+class Solution {
+    public int distinctSubseqII(String s) {
+        final long MOD = 1_000_000_007L;
+        long dp = 1;
+        long[] last = new long[26];
+        for (char c : s.toCharArray()) {
+            int idx = c - 'a';
+            long newDp = (2 * dp - last[idx] + MOD) % MOD;
+            last[idx] = dp;
+            dp = newDp;
+        }
+        return (int) ((dp - 1 + MOD) % MOD);
+    }
+}
