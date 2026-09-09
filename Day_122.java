e2a7eaaa2b87ec9c5ee009d2fb5a3b84808ce3d3
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-09-09 10:31:06 +0530

    Create Day_122.java

---
+++ Day_122.java
@@ -0,0 +1,33 @@
+/*
+You are given an integer n.
+Return the total number of commas used when writing all integers from [1, n] (inclusive) in standard number formatting.
+In standard formatting:
+A comma is inserted after every three digits from the right.
+Numbers with fewer than 4 digits contain no commas.
+
+Example 1:
+Input: n = 1002
+Output: 3
+Explanation:
+The numbers "1,000", "1,001", and "1,002" each contain one comma, giving a total of 3.
+
+Example 2:
+Input: n = 998
+Output: 0
+Explanation:
+​​​​​​​All numbers from 1 to 998 have fewer than four digits. Therefore, no commas are used.
+*/
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+        long start = 1000;
+        long commas = 1;
+        while (start <= n) {
+            long end = Math.min(n, start * 1000 - 1);
+            ans += (end - start + 1) * commas;
+            start *= 1000;
+            commas++;
+        }
+        return ans;
+    }
+}
