d1fc9465ea41ac6dc61528b9be58965b54c13e9d
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-09-11 10:03:15 +0530

    Create Day_124.java

---
+++ Day_124.java
@@ -0,0 +1,52 @@
+/*
+You are given an array of digits called digits. Your task is to determine the number of distinct three-digit even numbers that can be formed using these digits.
+Note: Each copy of a digit can only be used once per number, and there may not be leading zeros.
+
+Example 1:
+Input: digits = [1,2,3,4]
+Output: 12
+Explanation: The 12 distinct 3-digit even numbers that can be formed are 124, 132, 134, 142, 214, 234, 312, 314, 324, 342, 412, and 432. Note that 222 cannot be formed because there is only 1 copy of the digit 2.
+
+Example 2:
+Input: digits = [0,2,2]
+Output: 2
+Explanation: The only 3-digit even numbers that can be formed are 202 and 220. Note that the digit 2 can be used twice because it appears twice in the array.
+
+Example 3:
+Input: digits = [6,6,6]
+Output: 1
+Explanation: Only 666 can be formed.
+
+Example 4:
+Input: digits = [1,3,5]
+Output: 0
+Explanation: No even 3-digit numbers can be formed.
+*/
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int[] freq = new int[10];
+        for (int digit : digits) {
+            freq[digit]++;
+        }
+        int ans = 0;
+        for (int num = 100; num <= 998; num += 2) {
+            int x = num;
+            int[] need = new int[10];
+            while (x > 0) {
+                need[x % 10]++;
+                x /= 10;
+            }
+            boolean possible = true;
+            for (int d = 0; d <= 9; d++) {
+                if (need[d] > freq[d]) {
+                    possible = false;
+                    break;
+                }
+            }
+            if (possible) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
