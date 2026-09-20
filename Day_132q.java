64b686c43389fe449a6695980bab9a293e9f8b86
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-09-20 19:15:35 +0530

    Create Day_132q.java

---
+++ Day_132q.java
@@ -0,0 +1,31 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+Input: n = 1
+Output: ["()"]
+*/
+import java.util.*;
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+    private void backtrack(List<String> result,String current,int open,int close,int n) {
+        if (open == n && close == n) {
+            result.add(current);
+            return;
+        }
+        if (open < n) {
+            backtrack(result,current + "(",open + 1,close,n);
+        }
+        if (close < open) {
+            backtrack(result,current + ")",open,close + 1,n);
+        }
+    }
+}
