ab766e30fca294f8cde27d49c81c1fe2ba86f76c
Atherva Sahai <sahaiatherva2006@gmail.com>
2026-09-20 09:57:31 +0530

    Add longestPalindrome method to find palindromes
    Implement a method to find the longest palindromic substring in a given string.

---
+++ Day_132c.java
@@ -0,0 +1,42 @@
+/*
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+*/
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s.length() < 2) {
+            return s;
+        }
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // Odd-length palindrome
+            int len1 = expandFromCenter(s, i, i);
+            // Even-length palindrome
+            int len2 = expandFromCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    private int expandFromCenter(String s, int left, int right) {
+        while (left >= 0 &&
+               right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
